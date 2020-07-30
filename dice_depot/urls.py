7b48b602dc7b7a344ac84d5b7cbb734853a40ea3
Scrambles86 <craigvidler86@outlook.com>
2020-07-30 16:41:11 +0000

    Added rest imports to urls.py for api inclusion

--- dice_depot/urls.py
+++ dice_depot/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
     path('admin/', admin.site.urls),
