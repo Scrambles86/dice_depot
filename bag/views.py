eba7b370b0cf028d1ed5d4478168fd8187bb26b4
Scrambles86 <craigvidler86@outlook.com>
2020-08-02 20:51:25 +0000

    Created templates and bag folder in bag app

--- bag/views.py
+++ bag/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def view_bag(request):
-    """ Renders customers bag page """
+    """ Renders customers bag contents page """
     return render(request, 'bag/bag.html')
 
