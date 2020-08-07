94819f286e1e7db8f9af9a17c5b219e937c715d8
Scrambles86 <craigvidler86@outlook.com>
2020-08-07 20:05:34 +0000

    Created views.py file to render games in json

--- myapi/views.py
+++ myapi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+
+from .serializers import GameSerializer
+from .models import Game
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all().order_by('name')
+    serializer_class = GameSerializer
