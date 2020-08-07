f1fbb1dce96b1d32a746a048dfffdc536aa022b4
Scrambles86 <craigvidler86@outlook.com>
2020-08-07 14:24:35 +0000

    Added basic get function to views.py

--- dice_depot/views.py
+++ dice_depot/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_api_key.models import APIKey
+from rest_framework_api_key.permissions import HasAPIKey
+
+# Lists all games or create new entry in admin
+class GameList(APIView):
+
+    def get(self, request):
+        games = Games.objects.all()
+        pass
+
+    def post(self):
+        pass
