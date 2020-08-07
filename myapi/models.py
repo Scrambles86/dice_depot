20ae98570ff1bea9ee56671f8a7ddd5334e3d390
Scrambles86 <craigvidler86@outlook.com>
2020-08-07 19:45:57 +0000

    Syntax errors amended in models.py

--- myapi/models.py
+++ myapi/models.py
@@ -7,6 +7,9 @@ class Game(models.Model):
     publishers = models.CharField(max_length=60)
     year_published = models.IntegerField(max_length=4)
     max_players = models.IntegerField(max_length=4)
-    image_url = models.ImageField(upload_to"static/images")
+    image_url = models.ImageField(upload_to="static/images")
     msrp = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
