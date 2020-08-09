75082069aaf5e0c554a08d5de526fda8e2d395b5
Scrambles86 <craigvidler86@outlook.com>
2020-08-09 21:35:46 +0000

    Class added to allow admin editing

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Order, OrderLineItem
 
+class OrderLineItemAdminInline(admin, TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
 
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('order_numnber', 'date', 
