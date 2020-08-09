f0672e2b237db0e029fadf5085fd4edbb4ee9e7c
Scrambles86 <craigvidler86@outlook.com>
2020-08-09 15:37:48 +0000

    Created checkout app and added to settings

--- dice_depot/settings.py
+++ dice_depot/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'bag',
     'products',
     'myapi',
+    'checkout',
 ]
 
 MIDDLEWARE = [
--- myapi/views.py
+++ myapi/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import requests 
 from rest_framework import viewsets
 
 from .serializers import GameSerializer
