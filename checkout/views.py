7cf8cf46de1c9cd215c99834f91baee0be98a70a
Scrambles86 <craigvidler86@outlook.com>
2020-08-11 21:15:40 +0000

    Added order form redirect to views.py in checkout

--- checkout/views.py
+++ checkout/views.py
@@ -6,3 +6,11 @@ def checkout(request):
     if not bag:
         messages.error.request, "Your bag is empty"
         return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
