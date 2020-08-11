5be90da9c80b74bd459685420e209b3fad530c97
Scrambles86 <craigvidler86@outlook.com>
2020-08-11 21:54:06 +0000

    Crispy forms builtins added to settings

--- dice_depot/settings.py
+++ dice_depot/settings.py
@@ -80,11 +80,15 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # Required by allauth
+                'django.template.context_processors.request',   # Required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins' : [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
