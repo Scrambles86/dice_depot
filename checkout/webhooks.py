4cf6847a06b3c7a03185fb2c5a52201b2b152877
Scrambles86 <craigvidler86@outlook.com>
2020-08-13 22:12:16 +0000

    Stripe keys added to webhook function

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -2,7 +2,10 @@
 
 def webhook(request):
      """Listen for webhooks from Stripe"""
+    wh_secret = settings.STRIPE_WH_SECRET
+    stripe.api_key = settings.STRIPE_SECRET_KEY
 
+    
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
