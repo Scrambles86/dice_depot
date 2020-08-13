ae824b064b0a01177a3c38317a41cfede3ccb572
Scrambles86 <craigvidler86@outlook.com>
2020-08-13 14:34:34 +0000

    Added payment error variable in case of card errors

--- checkout/static/stripe_elements.js
+++ checkout/static/stripe_elements.js
@@ -3,3 +3,18 @@ var client_secret = $(#id_client_secret).text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var card = elements.create('card');
+card.mount('#card-element')
+
+// Event listener for card errors
+
+card.addEventListener('change', function(event) {
+    var paymentError = document.getElementById('payment-failed');
+    if (event.error) {
+        var html =
+            <span>${event.error.message}</span>
+
+        $(paymentError).html(html);
+    } else {
+        paymentError.textContent = '';
+    }
+});
