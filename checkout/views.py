59f88dce25dd878f927e40e868427326f18fd292
Scrambles86 <craigvidler86@outlook.com>
2020-08-13 16:06:01 +0000

    Created checkout confirm view for succesful orders

--- checkout/views.py
+++ checkout/views.py
@@ -18,6 +18,49 @@ def checkout(request):
         form_data = {
             'full_name': request.POST['full_name'],
             'email': request.POST['email'],
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
+            for item_id, item_data in bag.items():
+                try:
+                    product = Product.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                except Product.DoesNotExist:
+                    messages.error(request, (
+                        "Something went wrong! Please email us and we'll try to help!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_bag'))
+
+                    request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success', args=[order.order_number]))
+        else:
+            messages.error(request, 'Soemthing went wrong - please double check your information')
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "Your bag is empty!")
+            return redirect(reverse('products'))
         }
     else:
         bag = request.session.get('bag', {})
@@ -45,5 +88,46 @@ def checkout(request):
             'stripe_public_key': stripe_public_key,
             'client_secret': client_secret,
         }
+
+    return render(request, template, context)
+
+
+def checkout_confirm(request, order_number):
+    """
+    Handler for succesful orders
+    """
+
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    profile = UserProfile.objects.get(user=request.user)
+    order.user_profile = profile
+    order.save()
+
+    if save_info:
+        profile_data = {
+            'default_phone_number': order.phone_number,
+            'default_country': order.country,
+            'default_postcode': order.postcode,
+            'default_town_or_city': order.town_or_city,
+            'default_street_address1': order.street_address1,
+            'default_street_address2': order.street_address2,
+            'default_county': order.county,
+        }
+        user_profile_form = UserProfileForm(profile_data, instance=profile)
+        if user_profile_form.is_valid():
+            user_profile_form.save()
+
+    messages.success(request, f'Order confirmed! \
+        Your order number is {order_number}. We will email this \
+        confirmation to {order.email}.')
+
+    if 'bag' in request.session:
+        del request.session['bag']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
 
     return render(request, template, context)
