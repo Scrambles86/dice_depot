e10a9051932c2b3fcf61521438509d0b398db3fc
Scrambles86 <craigvidler86@outlook.com>
2020-08-13 21:53:44 +0000

    Created event error class to return an http response in the event of a payment error

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -5,3 +5,9 @@ class StripeWebhook:
 
     def __init__(self, request):
         self.request = request
+
+    def event_error(self, event):
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
