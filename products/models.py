dc9bbc74311250bc935de71049766c50201e388f
Scrambles86 <craigvidler86@outlook.com>
2020-08-17 20:51:17 +0000

    Added product model to models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=60)
+    publishers = models.CharField(max_length=60)
+    year_published = models.IntegerField()
+    max_players = models.IntegerField()
+    image_url = models.ImageField(upload_to="static/images")
+    msrp = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
