dd88281960dc8494bb1ce50b6ddcab02dc80e9ee
Scrambles86 <craigvidler86@outlook.com>
2020-08-20 17:21:06 +0000

    Product urls added to project level urls

--- dice_depot/urls.py
+++ dice_depot/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('bag/', include('bag.urls')),
+    path('products/', include('products.urls')),
     path('', include('myapi.urls')),
     path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
