dfa8a6fecf4b794284c6c77857cb802794d89d23
Scrambles86 <craigvidler86@outlook.com>
2020-08-20 17:16:10 +0000

    Created all products view in product app views.py

--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """ View that displays all available games from the json file """
+
+    return render(request, 'products/products.html', context)
