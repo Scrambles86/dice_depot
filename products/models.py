c513c312da6dbfb90f9d30f835eaf76a70f6cd69
Scrambles86 <craigvidler86@outlook.com>
2020-08-21 17:11:34 +0000

    Json file validated and added

--- products/models.py
+++ products/models.py
@@ -7,7 +7,7 @@ class Product(models.Model):
     primary_publisher = models.CharField(max_length=60)
     year_published = models.IntegerField()
     max_players = models.IntegerField()
-    image_url = models.ImageField(upload_to="static/images")
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     msrp = models.DecimalField(max_digits=6, decimal_places=2, default=0.0)
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
