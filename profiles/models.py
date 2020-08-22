984b8683c647887cf5facca3f59c939345fbcad1
Scrambles86 <craigvidler86@outlook.com>
2020-08-22 11:35:28 +0000

    Declared user profile model and docstring

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    """
+    Model to store the order history and delivery data 
+    for a user for repeat transactions
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
