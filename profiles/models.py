f7ee2be097dee943e8df2619631ece01cb68c837
Scrambles86 <craigvidler86@outlook.com>
2020-08-22 11:44:46 +0000

    Created receiver to amend or create user profiles on save

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django_countries.fields import CountryField
+
 # Create your models here.
 
 class UserProfile(models.Model):
@@ -9,10 +11,23 @@ class UserProfile(models.Model):
     for a user for repeat transactions
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=20, null=True, blank=True)
-    country = models.CharField(max_length=40, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
-    town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+@receiver(post_save, sender=User)
+def amend_user_profile(sender, instance, created, **kwargs):
+    """
+    Either updates user profile, 
+    or creates new one for new users
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.userprofile.save()
