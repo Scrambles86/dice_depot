11c895feb9fc7da6f16296089a87eb2af6929fef
Scrambles86 <craigvidler86@outlook.com>
2020-08-22 11:38:12 +0000

    Added delivery details from checkout models, changed null and blank tot reu to make saving optionl

--- profiles/models.py
+++ profiles/models.py
@@ -9,3 +9,10 @@ class UserProfile(models.Model):
     for a user for repeat transactions
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    country = models.CharField(max_length=40, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
