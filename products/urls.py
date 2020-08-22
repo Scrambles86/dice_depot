9e148de10823f72a6d29347a873c685c4393c2ad
Scrambles86 <craigvidler86@outlook.com>
2020-08-22 21:16:08 +0000

    Created product description template and added product image url

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='products')
-    path('<product_id>', views.product_description, name='product_description')
+    path('', views.all_products, name='products'),
+    path('<product_id>', views.product_description, name='product_description'),
 ]
