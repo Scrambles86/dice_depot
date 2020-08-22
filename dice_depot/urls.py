280e3b8e92220e75729a59a0ec6c7fd155346bc8
Scrambles86 <craigvidler86@outlook.com>
2020-08-22 12:13:46 +0000

    Added profile urls to project level

--- dice_depot/urls.py
+++ dice_depot/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('', include('myapi.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
