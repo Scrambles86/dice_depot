3889891324a96b6ffed1d3ae7147f6f8641e13fa
Scrambles86 <craigvidler86@outlook.com>
2020-08-23 18:27:38 +0000

    Post request handler added to views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -8,6 +9,12 @@ from .forms import UserProfileForm
 def profile(request):
     """User Profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile info updated')
 
     form = UserProfileForm(UserProfile, user=request.user)
     orders = profile.orders.all()
@@ -19,4 +26,3 @@ def profile(request):
     }
 
     return render(request, template, context)
-    
