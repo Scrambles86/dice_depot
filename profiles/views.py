c985b01d916c92587f848da4578fe363bf7ba37a
Scrambles86 <craigvidler86@outlook.com>
2020-08-23 18:19:45 +0000

    Added instances of user profile form into views.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 # Create your views here.
 
@@ -8,9 +9,14 @@ def profile(request):
     """User Profile"""
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders
     }
 
     return render(request, template, context)
+    
