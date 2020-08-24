3e1a45695a1eb8ea0c0809efc7115fe40d37066b
Scrambles86 <craigvidler86@outlook.com>
2020-08-24 17:20:58 +0000

    Created confirm order function to send email on order completion

--- checkout/webhook_handlers.py
+++ checkout/webhook_handlers.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+
+from .models import Order, OrderLineItem
+from products.models import Product
+from profiles.models import UserProfile
 
 
 class StripeWebhook:
 
     def __init__(self, request):
         self.request = request
+
+    def confirm_order(self, order):
+        """
+        Sends out confirmation email on completion of order
+        """
 
     def event_error(self, event):
 
