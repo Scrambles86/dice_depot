3c5697715f92d426af47a2b7aab8beaf3ef918bd
Scrambles86 <craigvidler86@outlook.com>
2020-08-24 20:25:34 +0000

    API app deleted - not in use

--- myapi/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Game
-
-class GameSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Game
-        fields = ('id', 'name', 'publishers', 'year_published', 'max_players', 'image_url', 'msrp', 'price')
