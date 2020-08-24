f4b520d38b635762c5e289c4e3b75e96e10a98bc
Scrambles86 <craigvidler86@outlook.com>
2020-08-24 20:12:20 +0000

    Added class to images for styling and added procuct description link

--- checkout/webhook_handlers.py
+++ checkout/webhook_handlers.py
@@ -17,6 +17,20 @@ class StripeWebhook:
         """
         Sends out confirmation email on completion of order
         """
+        confirm_email = order.email
+        subject = render_to_string(
+            'checkout/checkout_emails/subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/checkout_emails/body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL
+            [cust_email]
+        )
 
     def event_error(self, event):
 
