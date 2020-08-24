ea7c0d38b955b695f3a7d2e1b95d16e863c9b8e7
Scrambles86 <craigvidler86@outlook.com>
2020-08-24 20:25:11 +0000

    API formula moced to manage.py

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.db import models
+import requests
+import json
 
 
 def main():
@@ -19,3 +22,21 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# API Json Import
+
+ENDPOINT = os.environ.get('ENDPOINT')
+API_SECRET_KEY = os.environ.get('API_SECRET_KEY', '')
+API_CLIENT_ID = os.environ.get('API_CLIENT_ID', '')
+
+
+response = requests.get("https://api.boardgameatlas.com/api/search?client_id=yrOwuRj5mj")
+data = response.json()
+
+print(type(data))
+print(data)
+
+out_file = open("products/fixtures/products.json", "w")
+
+json.dump(data, out_file)
+
