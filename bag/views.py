05db95ff3ca517f813b419526a3ff8fd62d4682c
Scrambles86 <craigvidler86@outlook.com>
2020-08-24 14:10:23 +0000

    Created add item view

--- bag/views.py
+++ bag/views.py
@@ -6,3 +6,11 @@ def view_bag(request):
     """ Renders customers bag contents page """
     return render(request, 'bag/bag.html')
 
+def add_item(request, item_id):
+    """
+    Add game to bag in chosen quantity
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
