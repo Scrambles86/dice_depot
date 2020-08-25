07660a0dce874c9f4cfc8543a49c07dc611f50eb
Scrambles86 <craigvidler86@outlook.com>
2020-08-25 20:23:34 +0000

    Created sellGame model in sell app

--- sell/models.py
+++ sell/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+GAME_CONDITION = (
+    ('perfect,' 'PERFECT')
+    ('good', 'GOOD')
+    ('well used', 'WELL USED')
+    ('damaged', 'DAMAGED')
+)
+
+class sellGame(models.Model):
+    game_name = models.CharField(max_length=254, blank=False, Null=False)
+    sealed = models.BooleanField(label="Tick if sealed", help_text="Only tick this box if your game is still contained in it's original wrapping")
+    condition = models.CharField(max_length=8, option=GAME_CONDITION, default='good')
