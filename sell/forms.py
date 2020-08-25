fe0882630e66f903e8f1d9336db9508973d04c99
Scrambles86 <craigvidler86@outlook.com>
2020-08-25 20:29:15 +0000

    Created saleform class in forms.py

--- sell/forms.py
+++ sell/forms.py
@@ -1,2 +1,11 @@
 from django import forms
 from .models import sellGame
+
+class SaleForm(forms.ModelForm):
+    class Meta:
+        model = sellGame
+        fields = [
+            'game_name',
+            'sealed',
+            'condition',
+        ]
