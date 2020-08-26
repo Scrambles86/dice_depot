6227a2b0a36b266066faff56f6eb19455648cd82
Scrambles86 <craigvidler86@outlook.com>
2020-08-26 11:50:16 +0000

    Form parameters added into views.py, api formula commented out

--- manage.py
+++ manage.py
@@ -25,18 +25,18 @@ if __name__ == '__main__':
 
 # API Json Import
 
-ENDPOINT = os.environ.get('ENDPOINT')
-API_SECRET_KEY = os.environ.get('API_SECRET_KEY', '')
-API_CLIENT_ID = os.environ.get('API_CLIENT_ID', '')
+#ENDPOINT = os.environ.get('ENDPOINT')
+#API_SECRET_KEY = os.environ.get('API_SECRET_KEY', '')
+#API_CLIENT_ID = os.environ.get('API_CLIENT_ID', '')
 
 
-response = requests.get("https://api.boardgameatlas.com/api/search?client_id=yrOwuRj5mj")
-data = response.json()
+#response = requests.get("https://api.boardgameatlas.com/api/search?client_id=yrOwuRj5mj")
+#data = response.json()
 
-print(type(data))
-print(data)
+#print(type(data))
+#print(data)
 
-out_file = open("products/fixtures/products_full.json", "w")
+#out_file = open("products/fixtures/products_full.json", "w")
 
-json.dump(data, out_file)
+#json.dump(data, out_file)
 
--- sell/views.py
+++ sell/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.db import models
+from . models import sellGame
+from .forms import SaleForm
+
+from products.models import UserProfile
+
 
 # Create your views here.
 
 def sell(request):
     """ Renders sell page """
+    model = sellGame
+    form_class = SaleForm
+    template_name = 'sell/sell.html'
+
     return render(request, 'sell/sell.html')
 
