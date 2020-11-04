922f4ec0633d4ad4a8f7102332123c6fc5471dce
Scrambles86 <craigvidler86@outlook.com>
2020-11-04 16:18:58 +0000

    Added more product sorting to navbar

--- bag/urls.py
+++ bag/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
-    path('<product_id>', views.add_item, name='add_item'),
+    path('add/<product_id>/', views.add_item, name='add_item'),
 ]
