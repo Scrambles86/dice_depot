da7855f972cd40f4db1ecba9770b2bc9722cc4c8
Scrambles86 <craigvidler86@outlook.com>
2020-11-05 15:41:59 +0000

    Players and Age models created

--- products/models.py
+++ products/models.py
@@ -9,11 +9,8 @@ class Product(models.Model):
     description = models.CharField(max_length=1024, null=True, blank=True)
     description_preview = models.CharField(max_length=512, null=True, blank=True)
     year_published = models.IntegerField()
-    min_age = models.IntegerField()
-    min_players = models.IntegerField(default=0.0)
-    max_players = models.IntegerField(default=0.0)
-    min_playtime = models.IntegerField(default=0.0)
-    max_playtime = models.IntegerField(default=0.0)
+    players = models.ForeignKey('Players')
+    age = models.ForeignKey('Age')
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     thumb_url = models.URLField(max_length=1024, null=True, blank=True)
     url = models.URLField(max_length=1024, null=True, blank=True)
@@ -23,3 +20,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Players(models.Model):
+    min_players = models.IntegerField(default=0.0)
+    max_players = models.IntegerField(default=0.0)
+
+    def __str__(self):
+        return "%s %s" % (self.min_players, self.max_players)
+
+class Age(models.Model):
+    min_age = models.IntegerField()
+
+    def __str__(self):
+        return self.min_age
