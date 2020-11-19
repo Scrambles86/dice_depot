4cd8a870fea2673a08f322353245dd1831398830
Scrambles86 <craigvidler86@outlook.com>
2020-11-19 16:35:03 +0000

    Added url for edit bag

--- bag/urls.py
+++ bag/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<product_id>/', views.add_item, name='add_item'),
+    path('edit/<product_id>/', views.edit_bag, name='edit_bag'),
 ]
