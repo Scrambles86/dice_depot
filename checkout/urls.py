9e9e44fe1cae81bb51ebeb3c957da3f6ef842372
Scrambles86 <craigvidler86@outlook.com>
2020-11-21 21:15:17 +0000

    Url craeted for new view

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,6 +5,7 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_confirm/<order_number>', views.checkout_confirm, name='checkout_confirm'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
     path('wh/', webhook, name='webhook'),
 
 ]
