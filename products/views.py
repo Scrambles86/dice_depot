53cfa1d14b69cc27e632abd66b6447ca3f705049
Scrambles86 <craigvidler86@outlook.com>
2020-11-23 22:36:03 +0000

    Created add product view

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Age, Players
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -57,3 +58,13 @@ def product_description(request, product_id):
     }
 
     return render(request, 'products/product_description.html', context)
+
+
+def add_product(request):
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context : {
+        'form': form,
+    }
+
+return render(request, template, context)
