f3083c8eb38c7ceeb10704b8d28eed989dc49fe1
Scrambles86 <craigvidler86@outlook.com>
2020-11-23 21:33:41 +0000

    Created product form

--- sell/models.py
+++ sell/models.py
@@ -11,10 +11,11 @@ GAME_CONDITION = (
 
 class Game(models.Model):
     game_name = models.CharField(max_length=254, blank=False)
-    model_pic = models.ImageField(upload_to = 'user_game_images/', default = '')
+    model_pic = models.ImageField(upload_to='user_game_images/', default='')
     sealed = models.BooleanField(help_text="Only tick this box if your game is still contained in it's original wrapping")
     condition = models.CharField(max_length=25, choices=GAME_CONDITION, default='good')
     game_description = models.TextField(max_length=512, help_text="Please provide a quick description of your game's quality", null=True, blank=True)
+    email = models.EmailField(max_length=254, null=False, blank=False)
 
     def __str__(self):
         return self.game_name
