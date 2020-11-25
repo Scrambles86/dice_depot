cab566f383ce450a0ae08d866e8c9aeeed2cc6ab
Scrambles86 <craigvidler86@outlook.com>
2020-11-25 14:30:38 +0000

    Added login requirements to product editing views

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Product, Age, Players
@@ -59,7 +60,7 @@ def product_description(request, product_id):
 
     return render(request, 'products/product_description.html', context)
 
-
+@login_required
 def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
@@ -79,6 +80,7 @@ def add_product(request):
 
     return render(request, template, context)
 
+@login_required
 def edit_product(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
@@ -102,7 +104,7 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
-
+@login_required
 def delete_product(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
