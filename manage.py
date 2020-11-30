e7c3feab96e8821fcc231b41d44732470cd47d14
Scrambles86 <craigvidler86@outlook.com>
2020-11-30 23:30:40 +0000

    Removed json dump function from manage.py

--- manage.py
+++ manage.py
@@ -23,20 +23,5 @@ def main():
 if __name__ == '__main__':
     main()
 
-# API Json Import
 
-#ENDPOINT = os.environ.get('ENDPOINT')
-#API_SECRET_KEY = os.environ.get('API_SECRET_KEY', '')
-#API_CLIENT_ID = os.environ.get('API_CLIENT_ID', '')
-
-
-#response = requests.get("https://api.boardgameatlas.com/api/search?client_id=yrOwuRj5mj")
-#data = response.json()
-
-#print(type(data))
-#print(data)
-
-#out_file = open("products/fixtures/products_full.json", "w")
-
-#json.dump(data, out_file)
 
